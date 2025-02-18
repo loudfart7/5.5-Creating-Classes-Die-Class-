550a10d5f73d812fc7df1e65a283a1f7439bb40b
Chloe Vandeworp <339639510@GoTVDSB.ca>
2025-02-18 14:39:35 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _5._5_Creating_Classes__Die_Class_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Die die1, die2;
+            die1 = new Die();
+            die2 = new Die(-10);
+
+            Console.WriteLine(die1);
+            die1.DrawDie();
+
+            die1.RollDie();
+            Console.WriteLine(die1);
+            die1.DrawDie();
+
+
+        }
+    }
+}
