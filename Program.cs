7970a66c6057739ab0e55fbab80d685919c9d82e
Chloe Vandeworp <339639510@GoTVDSB.ca>
2025-04-07 13:47:30 -0400

    done

--- Program.cs
+++ Program.cs
@@ -6,15 +6,44 @@
         {
             Die die1, die2;
             die1 = new Die();
-            die2 = new Die(-10);
+            die2 = new Die();
 
-            Console.WriteLine(die1);
+            if (die1.Roll == die2.Roll)
+                Console.WriteLine("DOUBLES");
+            else
+                Console.WriteLine("NOT DOUBLES");
+
+            Console.WriteLine();
+
+            if (die1.Roll + die2.Roll == 7)
+                Console.WriteLine("THE SUM OF YOUR DIE IS SEVEN");
+            else
+                Console.WriteLine("THE SUM OF YOUR NUMBERS IS NOT SEVEN");
+
+            Console.WriteLine();
+
+            if (die1.Roll==1 && die2.Roll == 1)
+                Console.WriteLine("SNAKE EYES!");
+
+            if ((die1.Roll + die2.Roll) % 2 == 0)
+                Console.WriteLine("EVEN SUM");
+
+            Console.WriteLine();
+
+            if (die1.Roll == die2.Roll + 1 || die2.Roll == die1.Roll + 1)
+                Console.WriteLine("SEQUENTIAL ROLL");
+            else
+                Console.WriteLine("NOT SEQUENTIAL");
+
+            Console.WriteLine();
+
             die1.DrawDie();
+            die2.DrawDie();
 
-            die1.RollDie();
-            Console.WriteLine(die1);
-            die1.DrawDie();
-
+            //die1.RollDie();
+            //Console.WriteLine(die1);
+            //die1.DrawDie();
+            Console.ReadLine();
 
         }
     }
